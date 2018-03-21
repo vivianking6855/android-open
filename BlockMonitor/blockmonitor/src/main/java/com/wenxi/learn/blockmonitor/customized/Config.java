08fa24f3db5ef4e4e8ce4aabebeeafaf28d43cdc
vivian_sun <vivian_sun@asus.com>
2018-03-21 18:49:33 +0800

    modify dump block time

--- BlockMonitor/blockmonitor/src/main/java/com/wenxi/learn/blockmonitor/customized/Config.java
+++ BlockMonitor/blockmonitor/src/main/java/com/wenxi/learn/blockmonitor/customized/Config.java
@@ -5,7 +5,7 @@ package com.wenxi.learn.blockmonitor.customized;
  */
 
 public class Config implements IConfig {
-    private static final long TIME_BLOCK = 1000L;
+    private static final long TIME_BLOCK = 80 ;// 80ms = 5*16.6ms
     private static final String PATH = "/block_log/";
 
     @Override
