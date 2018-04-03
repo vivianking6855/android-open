d0a2477c5260ca723395e777238a50b9b5126de2
vivian_sun <vivian_sun@asus.com>
2018-04-03 15:36:36 +0800

    add base fragment

---
+++ BlockMonitor/blockmonitor/src/main/java/com/learn/blockmonitor/share/base/BaseMVPLazyFragment.java
@@ -0,0 +1,24 @@
+package com.learn.blockmonitor.share.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created on 2018/2/28.
+ * lazy fragment: only load data if user visible fragment and data not load complete
+ * you must setDataLoadCompleted(true), when your data load complete, otherwise it will load every time
+ */
+public abstract class BaseMVPLazyFragment<V, T extends BasePresenter<V>> extends BaseLazyFragment {
+    protected T mPresenter; // presenter
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // create presenter
+        mPresenter = createPresenter();
+        mPresenter.attachReference((V) this);
+    }
+
+    protected abstract T createPresenter();
+
+}
